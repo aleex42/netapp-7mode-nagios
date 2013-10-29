25d58a85fcc5eb7aad387bf3f878d6b36b3ce059
Alexander Krogloth <git@krogloth.de>
2013-10-29 11:23:36 +0100

    Added Uptime

---
+++ check_7mode_uptime.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# --
+# check_7mode_uptime.sh Check NetApp System Uptime
+# Copyright (C) 2013 noris network AG, http://www.noris.net/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
+# --
+ 
+HOST=$1
+COMMUNITY=$2
+MUST_UPTIME=$3
+ 
+UPTIME=$(/usr/bin/snmpwalk -v 2c -c $COMMUNITY $HOST .1.3.6.1.2.1.1.3.0 | awk -F \( '{ print $2 }' | awk -F \) '{ print $1 }')
+ 
+UPTIME_MINUTES=$(echo $UPTIME/6000 | bc)
+UPTIME_DAYS=$(echo $UPTIME_MINUTES/1440 | bc -l)
+ROUNDED_DAYS=$(printf %.1f $UPTIME_DAYS)
+ 
+if [ $UPTIME_MINUTES -lt $MUST_UPTIME ]; then
+        echo "CRITICAL - Uptime only $UPTIME_MINUTES minutes"
+        exit 2
+else
+        echo "OK - Uptime $UPTIME_MINUTES minutes (~ $ROUNDED_DAYS days)"
+        exit 0
+fi
